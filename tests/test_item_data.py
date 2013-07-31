751b5afd8fb761f5303fe386b9b06e09443dad12
Mark Wingerd <markwingerd@yahoo.com>
2013-07-30 19:54:27 -0700

    Added test for adding an item to the db.

--- tests/test_item_data.py
+++ tests/test_item_data.py
@@ -47,11 +47,39 @@ class TestItemDatabase(unittest.TestCase):
 				'price_aur', 'base_damage']
 		self.db_cursor.execute('select * from weapons')
 		weapons = map(lambda item: item[0], self.db_cursor.description)
-		print self.db_cursor.description
 
 		self.assertItemsEqual(dropsuits, expected_dropsuits)
 		self.assertItemsEqual(modules, expected_modules)
 		self.assertItemsEqual(weapons, expected_weapons)
+
+	def test_add_item(self):
+		""" Tests that items can be added to tables. """
+		dropsuit_db_data = {'id': 0, 'name': 'God Suit', 'race': 'Jove', 
+				'meta_level': 42, 'price_isk': 60000, 'price_aur': 600}
+		module_db_data = {'id': 0, 'name': 'Duct Tape', 'meta_level': 42,
+				 'price_isk': 5500, 'price_aur': 60, 'slot_type': 'Hi Slot'}
+		weapon_db_data = {'id': 0, 'name': 'Ban Hammer', 'meta_level': 42,
+				 'price_isk': 36, 'price_aur': 6, 'base_damage': 9999}
+
+		self.itemdb.add(itemdb.DB_DROPSUIT, dropsuit_db_data)
+		self.itemdb.add(itemdb.DB_MODULE, module_db_data)
+		self.itemdb.add(itemdb.DB_WEAPON, weapon_db_data)
+
+		expected_dropsuit_values = dropsuit_db_data.values()
+		self.db_cursor.execute('SELECT * FROM dropsuits WHERE id=?', (0,))
+		dropsuit_values = self.db_cursor.fetchone()
+
+		expected_module_values = module_db_data.values()
+		self.db_cursor.execute('SELECT * FROM modules WHERE id=?', (0,))
+		module_values = self.db_cursor.fetchone()
+
+		expected_weapon_values = weapon_db_data.values()
+		self.db_cursor.execute('SELECT * FROM weapons WHERE id=?', (0,))
+		weapon_values = self.db_cursor.fetchone()
+
+		self.assertItemsEqual(dropsuit_values, expected_dropsuit_values)
+		self.assertItemsEqual(module_values, expected_module_values)
+		self.assertItemsEqual(weapon_values, expected_weapon_values)
 
 
 if __name__ == '__main__':
