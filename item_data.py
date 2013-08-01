d29177ddc669b000a2bb36b5ca300f615bf942c0
Mark Wingerd <markwingerd@yahoo.com>
2013-07-31 18:57:57 -0700

    Added 3 ItemDatabase.add_ methods. Passed tests.

--- item_data.py
+++ item_data.py
@@ -11,22 +11,53 @@ PARAMSTYLE = 'qmark'
 
 
 class ItemDatabase():
+
 	def __init__(self, database_file='items.db'):
 
 		self.conn = sqlite3.connect(database_file)
 		self.c = self.conn.cursor()
 		try:
 			self.c.execute('''CREATE TABLE dropsuits
-				(id int, name text, race text, meta_level int, 
-				price_isk int, price_aur int)''')
+				(id int, name text, meta_level int, price_isk int, price_aur int, race text)''')
 			self.c.execute('''CREATE TABLE modules
-				(id int, name text, meta_level int, price_isk int, 
-				price_aur int, slot_type text)''')
+				(id int, name text, meta_level int, price_isk int, price_aur int, slot_type text)''')
 			self.c.execute('''CREATE TABLE weapons
-				(id int, name text, meta_level int, price_isk int, 
-				price_aur int, base_damage text)''')
+				(id int, name text, meta_level int, price_isk int, price_aur int, base_damage text)''')
 		except sqlite3.OperationalError as e:
 			print e
+
+	def add_dropsuit(self, attrib_dict):
+		# Get the order of the table columns
+		self.c.execute('SELECT * FROM dropsuits')
+		columns = map(lambda item: item[0], self.c.description)
+		# Create a list of attrib_dict values in the correct order.
+		item_values = []
+		for key in columns:
+			item_values.append(attrib_dict[key])
+
+		self.c.execute('INSERT INTO dropsuits VALUES (?,?,?,?,?,?)', item_values)
+
+	def add_module(self, attrib_dict):
+		# Get the order of the table columns
+		self.c.execute('SELECT * FROM modules')
+		columns = map(lambda item: item[0], self.c.description)
+		# Create a list of attrib_dict values in the correct order.
+		item_values = []
+		for key in columns:
+			item_values.append(attrib_dict[key])
+
+		self.c.execute('INSERT INTO modules VALUES (?,?,?,?,?,?)', item_values)
+
+	def add_weapon(self, attrib_dict):
+		# Get the order of the table columns
+		self.c.execute('SELECT * FROM weapons')
+		columns = map(lambda item: item[0], self.c.description)
+		# Create a list of attrib_dict values in the correct order.
+		item_values = []
+		for key in columns:
+			item_values.append(attrib_dict[key])
+
+		self.c.execute('INSERT INTO weapons VALUES (?,?,?,?,?,?)', item_values)
 
 if __name__ == '__main__':
 	itemdb = ItemDatabase()
